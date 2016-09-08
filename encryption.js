046b30842e00b37d8b30c899b7af06b76a7b4434
Mark Old <dlom234@gmail.com>
2016-09-08 14:42:41 -0700

    Fix null padding issues to pass tests

--- encryption.js
+++ encryption.js
@@ -1,13 +1,18 @@
 var crypto = require('crypto');
 var iv = new Buffer("");
 
+var PADDING_LENGTH = 16;
+var PADDING = Array(PADDING_LENGTH).join("\0");
+
 function createCryptor(key) {
     key = new Buffer(key);
     return function encrypt(data) {
         var cipher = crypto.createCipheriv("bf-ecb", key, iv);
+        cipher.setAutoPadding(false);
+        var padLength = PADDING_LENGTH - (data.length % PADDING_LENGTH);
         try {
             return Buffer.concat([
-                cipher.update(data),
+                cipher.update(data + PADDING.substr(0, padLength)),
                 cipher.final()
             ]);
         } catch (e) {
@@ -20,6 +25,7 @@ function createDecryptor(key) {
     key = new Buffer(key);
     return function decrypt(data) {
         var cipher = crypto.createDecipheriv("bf-ecb", key, iv);
+        cipher.setAutoPadding(false);
         try {
             return Buffer.concat([
                 cipher.update(data),
@@ -42,6 +48,6 @@ exports.decrypt = function(password, ciphered) {
 exports.encrypt = function(password, plain) {
     var blowfish = createCryptor(password);
     var buff = blowfish(plain);
- 
+
     return buff;
 };
